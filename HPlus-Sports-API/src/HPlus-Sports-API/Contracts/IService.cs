652708749d5448040d42445c5c0fdb5be072f590
Chris Woodruff <cwoodruff@live.com>
2016-09-19 07:44:46 -0400

    upgraded to ASP.NET Core 1.0.1 and added new services to prep for in-memory caching

---
+++ HPlus-Sports-API/src/HPlus-Sports-API/Contracts/IService.cs
@@ -0,0 +1,13 @@
+﻿using HPlusSportsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPlusSportsAPI.Contracts
+{
+    public interface IService<T> where T : class
+    {
+        IEnumerable<T> Get();
+    }
+}
---
+++ HPlus-Sports-API/src/HPlus-Sports-API/Services/CustomerService.cs
@@ -0,0 +1,21 @@
+﻿using HPlusSportsAPI.Contracts;
+using HPlusSportsAPI.Models;
+using System.Collections.Generic;
+
+namespace HPlusSportsAPI.Services
+{
+    public class CustomerService : IService<Customer>
+    {
+        private H_Plus_SportsContext ds;
+
+        public CustomerService(H_Plus_SportsContext d)
+        {
+            ds = d;
+        }
+
+        public IEnumerable<Customer> Get()
+        {
+            return ds.Customer;
+        }
+    }
+}
